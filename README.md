ebc7dbf6e0460484cefbf77eac03a886b99eb2ff
Lucas Gomes <201810451@aluno.fjn.edu.br>
2020-06-22 20:51:07 -0300

    Criação da estrutura inicial do projeto, inclusão da arquitetura básica e criação do README

---
+++ README.md
@@ -0,0 +1,16 @@
+#projeto-es1
+
+Projeto da disciplina de Engenharia de Software 1, do curso de Sistemas de Informação, desenvolvido por alunos do 5 semestre.
+Ferramentas utilizadas
+Spring Boot
+Spring MVC
+Spring Web
+Spring Security
+Spring Data
+Spring Session
+Spring Boot DevTools
+PostgreSQL
+Gradle
+Thymeleaf
+Lombok
+Bootstrap
---
+++ src/main/java/com/lacrocant/lacrocant/LaCrocantApplication.java
@@ -0,0 +1,13 @@
+package com.lacrocant.lacrocant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaCrocantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaCrocantApplication.class, args);
+	}
+
+}
