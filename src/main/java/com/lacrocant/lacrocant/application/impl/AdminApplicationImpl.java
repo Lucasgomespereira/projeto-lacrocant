855986d8bc34311f722ddab7845e1df9724c2589
Lucas Gomes <201810451@aluno.fjn.edu.br>
2020-06-25 00:00:52 -0300

    Criação do template index, que será parte da area do administrador

--- src/main/java/com/lacrocant/lacrocant/application/impl/AdminApplicationImpl.java
+++ src/main/java/com/lacrocant/lacrocant/application/impl/AdminApplicationImpl.java
@@ -25,7 +25,6 @@ public class AdminApplicationImpl implements AdminApplication {
 
     @Override
     public Admin findById(String id) {
-        // TODO Auto-generated method stub
         return null;
     }
 
